d79c914902eb69c067a09f8b3617288ad0f7a1db
Lorae <10428186+lorae@users.noreply.github.com>
2025-09-30 13:42:09 -0400

    feat: add import-data.R to run-all.R

--- run-all.R
+++ run-all.R
@@ -1,1 +1,7 @@
-# placeholder
+# run-all.R
+#
+# This file sequentially runs all scripts necessary for the project to run.
+# Be sure to follow Parts A and B of the setup instructions outlined in README.md
+# prior to running this script.
+
+source("import-data.R") # Run time: roughly 30 minutes
