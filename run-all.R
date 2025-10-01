8ae1ecbd50b357873e0e04610c4fee9af2b04e19
Lorae <10428186+lorae@users.noreply.github.com>
2025-10-01 11:59:53 -0400

    fix: update run-all

--- run-all.R
+++ run-all.R
@@ -4,4 +4,9 @@
 # Be sure to follow Parts A and B of the setup instructions outlined in README.md
 # prior to running this script.
 
-source("import-ipums.R") # Run time: about 30-45 minutes
+# Setup
+source("src/scripts/import-ipums.R") # Run time: about 30-45 minutes
+source("src/scripts/process-ipums.R") # Run time: 2 mins
+
+# Figures
+source("src/figures/fig01-household-size-year-nativity-line.R")
