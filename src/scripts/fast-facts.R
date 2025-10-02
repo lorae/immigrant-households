09e2fd758e711100cb2c86e85e3f8811c509af9b
Lorae <10428186+lorae@users.noreply.github.com>
2025-10-02 17:46:25 -0400

    feat: update fast facts recent year to 2020

--- src/scripts/fast-facts.R
+++ src/scripts/fast-facts.R
@@ -14,7 +14,7 @@ ipums_db <- tbl(con, "ipums_person")
 # ------ Step 1: Facts ----- #
 
 # "In 1970, the average person lived in a household that included x people 
-# (including themselves). By 2023, this had fallen to x people, a decline of x%.
+# (including themselves). By 2020, this had fallen to x people, a decline of x%.
 hhsize_decade <- crosstab_mean(
   data = ipums_db |> filter(GQ %in% c(0,1,2)),
   value = "NUMPREC",
@@ -25,9 +25,9 @@ hhsize_decade <- crosstab_mean(
 hhsize_decade
 
 hhsize1970 <- hhsize_decade |> filter(decade == 1970) |> pull(weighted_mean)
-hhsize2023 <- hhsize_decade |> filter(decade == 2023) |> pull(weighted_mean)
+hhsize2020 <- hhsize_decade |> filter(decade == 2020) |> pull(weighted_mean)
 
-((hhsize1970 - hhsize2023) / hhsize1970)*100
+((hhsize1970 - hhsize2020) / hhsize1970)*100
 
 # We exclude individuals living in institutional settings, such as prisons and 
 # nursing homes, removing between ____% and ____% of the population across years. 
